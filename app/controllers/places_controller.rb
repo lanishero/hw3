bcbab92544b5d7c98c000915a1472ce4c0339279
lanishero <lanchen.song@kellogg.northwestern.edu>
2022-02-20 21:39:30 +0000

    44

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -8,9 +8,19 @@ class PlacesController < ApplicationController
         @place = Place.new
     end
 
+    def show
+        @place = Place.find(params["id"])
+    end
+
     def create
     @place = Place.new(params["place"])
     @place.save
+    redirect_to "/places"
+    end
+
+    def destroy
+    @place = Place.find(params["id"])
+    @place.destroy
     redirect_to "/places"
     end
 
